4f01f2ba899cac1156ff95fb5b7910be89565abd
Divesh <80365717+nekoism@users.noreply.github.com>
2022-11-22 16:15:42 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,13 +1,9 @@
 module.exports.run = async (bot, message, args) => {
-
-
   const m = await message.channel.send("ping?");
   m.edit(`Pong! ${m.createdTimestamp - message.createdTimestamp}ms`);
-
 }
 
 module.exports.help ={
   name: "ping", 
   aliases: ["p"]
 }
-
